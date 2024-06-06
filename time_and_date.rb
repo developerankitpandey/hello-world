4b9758105c7b4ba9f2889dc74bbf7afba81473da
ankit <iamankitpandey801@gmail.com>
2024-06-06 15:53:30 +0530

    my first commit

---
+++ time_and_date.rb
@@ -0,0 +1,29 @@
+# time1 = Time.new 
+# puts "current_time: #{ time1 } " 
+
+# time = Time.new 
+# puts time.year
+# puts time.month 
+# puts time.day 
+# puts time.wday 
+# puts time.yday
+# puts time.hour
+# puts time.min
+# puts time.sec
+# puts time.usec
+# puts time.zone
+
+# time = Time.new 
+# puts time.zone 
+# puts time.year 
+# puts time.day 
+# puts time.hour 
+# puts time.wday 
+# puts time.yday 
+# puts time.zone 
+# puts time.sec
+# puts time.usec 
+
+require 'date'
+# puts Time.new(2019,4,6).to_date
+# puts Date.strptime('07-08-2018', '%d-%m-%Y')
